555a1933cf713b5f0ca852acb404640071fc658f
Abi Travers <atravers4007@gmail.com>
2017-11-16 15:51:14 +0000

    Testing that the number given is not zero or less than 1 (negative)

--- spec/CheckInputSpec.js
+++ spec/CheckInputSpec.js
@@ -9,37 +9,35 @@ describe("checkInput", function (){
     checkInput = new CheckInput();
   });
 
-  it("Checks the input number which has been passed to it is not a string", function(){
-    checkInput.isString("not-number");
-    expect(checkInput.inputNrCorrect).toEqual(false);
-  });
-
-  // it("Checks the input number which has been passed to it is not a string", function(){
-  //   checkInput.isString(4);
-  //   expect(checkInput.inputNrCorrect).toEqual(true);
-  // });
-
-  it("Checks the input number is a positive number which is higher than 1", function(){
-    checkInput.isString(-1);
-    checkInput.validNumber(-1)
-    expect(checkInput.inputNrCorrect.toEqual(false));
-  });
-
-  it("Checks the input number is a positive number which is higher than 1 and lower than infinity", function(){
-    checkInput.isString(8);
+  it("Returns true if the User Number (input number) which has been passed to it is a positive number which is higher than 1 and lower than infinity", function(){
     checkInput.validNumber(8)
     expect(checkInput.inputNrCorrect.toEqual(true));
   });
 
-  it("Checks the input number is a positive number which is lower than infinity", function(){
-    checkInput.isString(100000000000000000000);
-    checkInput.validNumber(100000000000000000000)
-    expect(checkInput.inputNrCorrect.toEqual(false));
+  it("Checks the User Number is not a string. It returns false if it is a string.", function(){
+    checkInput.isString("not-number");
+    expect(checkInput.inputNrCorrect).toEqual(false);
   });
 
-  it("Checks the input number is not a decimal number, if it is it sets this.inputNrCorrect to false", function(){
-    checkInput.isString(9.88);
+
+  it("Returns false if the User Number is less than one. Negative value test", function(){
+    checkInput.validNumber(-1)
+    expect(checkInput.inputNrCorrect).toEqual(false);
+  });
+
+  it("Returns false if the User Number is less than one. Zero value test", function(){
+    checkInput.validNumber(0)
+    expect(checkInput.inputNrCorrect).toEqual(false);
+  });
+
+
+  it("Returns false if the User Number is a number lower than infinity", function(){
+    checkInput.validNumber(100000000000000000000)
+    expect(checkInput.inputNrCorrect).toEqual(false);
+  });
+
+  it("Returns false if the User Number is not a decimal number, if it is it sets this.inputNrCorrect to false", function(){
     checkInput.validNumber(9.88)
-    expect(checkInput.inputNrCorrect.toEqual(false));
+    expect(checkInput.inputNrCorrect).toEqual(false);
   });
 });
