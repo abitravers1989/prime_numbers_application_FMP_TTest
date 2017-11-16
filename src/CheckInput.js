0c2efa9e1f64e4c77f514af27508e9800c8f0924
Abi Travers <atravers4007@gmail.com>
2017-11-16 15:35:58 +0000

    Check input module now checks if number is string or above 1

--- src/CheckInput.js
+++ src/CheckInput.js
@@ -1,27 +1,24 @@
 var CheckInput = function() {
-  // Why is this.inputNrCorrect undefined here?
+  // Why is this.inputNrCorrect undefined here?... because this is the window
   this.inputNrCorrect = false;
 };
 
 // checking it is not a string
 CheckInput.prototype.isString = function (userInput){
-   if (isNaN(userInput)) this.inputCorrect = false;
+   if (isNaN(userInput)) this.inputNrCorrect = false;
    console.log('String checked called');
    // return this.inputCorrect
 };
 
 CheckInput.prototype.validNumber = function (userInput){
-   // this.isString(userInput)
-   // console.log(userInput)
-   // console.log("1")
-   // console.log(this.inputCorrect)
-   if (userInput >= 1) this.inputCorrect = true;
+   if (userInput >= 1) this.inputNrCorrect = true;
  };
 
 //
 // module.exports.CheckInput = CheckInput;
 
 checkInput = new CheckInput();
+
 
 checkInput.validNumber(8)
 checkInput.inputNrCorrect
