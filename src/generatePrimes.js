4c8cb78a85641dde889a9a8f4fbeb7dd1bf65043
Abi Travers <atravers4007@gmail.com>
2017-11-17 15:57:08 +0000

    Some workings out / understanding the generate prime functionality.. Not working / completed just to show my process

--- src/generatePrimes.js
+++ src/generatePrimes.js
@@ -1,49 +1,28 @@
 var GeneratePrimes = function () {
-  this.primesarray = [];
+  // this.primesarray = [];
 };
 
 GeneratePrimes.prototype.createArrayofPrimes = function (inputNumber) {
+  primesarray = [];
   var number = 2;
-  Loop from number to inputNumber
-
-  check each of these is not divisible by 2 and 3
-
-  i = 2
   while(array.length < inputNumber) {
-    if num.isPrime   array << num
-    i++
+    if (num.isPrime()) { primesarray << num }
+    number+=1;
   }
-}
-
-}
-
-
-GeneratePrimes.Prototype.isPrime = function(num){
-  console.log(num)
-  for(var i = 2; i < num; i++)
-   if(num % i === 0) return false;
- return num !== 1;
+  return primesarray
 }
 
 
-
-
-
-function isPrime(n) {
-
-   // If n is less than 2 or not an integer then by definition cannot be prime.
-   if (n < 2) {return false}
-   if (n != Math.round(n)) {return false}
-
-   // Now assume that n is prime, we will try to prove that it is not.
-   var isPrime = true;
-
-   // Now check every whole number from 2 to the square root of n. If any of these divides n exactly, n cannot be prime.
-   for (var i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i == 0) {isPrime = false}
-   }
-
-   // Finally return whether n is prime or not.
-   return isPrime;
-
+GeneratePrimes.Prototype.isPrime = function(n){
+  // If n is less than 2 or not an integer then by definition cannot be prime.
+  if (n < 2) {return false}
+  if (n != Math.round(n)) {return false}
+  // Now assume that n is prime, we will try to prove that it is not.
+  var isPrime = true;
+  // Now check every whole number from 2 to the square root of n. If any of these divides n exactly, n cannot be prime.
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+     if (n % i == 0) {isPrime = false}
+  }
+  // Finally return whether n is prime or not.
+  return isPrime;
 }
