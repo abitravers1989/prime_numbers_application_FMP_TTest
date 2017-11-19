9a20657f61d529f83970ecf666f04edb99033271
Abi Travers <atravers4007@gmail.com>
2017-11-19 09:31:28 +0000

    Working through the multiplication tables functionality

--- src/generateMultiplicationTs.js
+++ src/generateMultiplicationTs.js
@@ -1,58 +1,24 @@
 var GenerateMultiplicationTs = function(){
-
+    this.primesArray = "unset";
 };
 
 GenerateMultiplicationTs.prototype.gMTables = function(primesArray){
-  primesArray.forEach((number)=> {
-    array = new Array
-    counter = 1
-    while (primesArray.length < counter) {
-    array.push(item * counter)
-    counter +=1
-  };
-  primesArray.push(array)
-  console.log(primesArray)
- });
-};
-
-
-
-
-function myFunction(primesArray) {
-  counter = 1
   var length = primesArray.length
-
-while (length < counter) {
     primesArray.forEach((number)=> {
+      counter = 1
+      while (length >= counter) {
       array = new Array
-        console.log(counter)
-        console.log(primesArray.length)
-      array.push(item * counter)
+      array.push(number * counter)
       counter +=1
+      // console.log(array)
     };
     primesArray.push(array)
-    console.log(primesArray)
   });
+    this.primesArray = primesArray;
+    return this.primesArray;
 };
 
-myFunction([2,3,5,7,11])
-
-primesArray.reduce((all, item, index)=>{
- all[item.email] = item.name;
- return all;
-}, {primesArray} );
-
-var primesArray = [2,3,5,7,11];
-
-primesArray.reduce((all, item, index)=>{
-
- counter = 1
- array.push(item * counter)
-
- // console.log(item)
- return all;
- console.log(all)
-}, { console.log(primesArray) } );
-
+gm = new GenerateMultiplicationTs();
+gm.gMTables([2,3,5,7,11]);
 
 module.exports.GenerateMultiplicationTs = GenerateMultiplicationTs;
