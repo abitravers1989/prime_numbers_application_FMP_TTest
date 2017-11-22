6faa7533c1bd85963d6e3e21051ddd45db0f139d
Abi Travers <atravers4007@gmail.com>
2017-11-22 08:58:29 +0000

    Adding easy table functionality. Spiking. Needs refactoring

--- src/outputTable.js
+++ src/outputTable.js
@@ -1,1 +1,17 @@
 var Table = require('easy-table')
+
+var data = [ [ null, 2, 3, 5 ],
+[ 2, 4, 6, 8 ],
+[ 3, 6, 9, 12 ],
+[ 5, 10, 15, 20 ] ];
+
+// var t = new Table
+//
+// data.forEach(function(product){
+//    t.cell(data[1][0][2])
+//    t.cell(data[2][0][2])
+//    t.cell(data[3][0][2])
+//    t.newRow();
+// })
+
+console.log(Table.print(data))
