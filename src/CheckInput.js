2085ef8776b5fac17ddcdbcac0cc6c6b0756c904
Abi Travers <atravers4007@gmail.com>
2017-11-24 09:04:46 +0000

    Making functions in check input module consistent

--- src/CheckInput.js
+++ src/CheckInput.js
@@ -7,9 +7,23 @@ CheckInput.prototype._isNotString = function (userInput){
   if (isNaN(userInput)) this.inputNrCorrect = false;
 };
 
+CheckInput.prototype._isNotInfinate = function (userInput){
+  if ((isFinite(userInput) === true)) this.inputNrCorrect = true;
+};
+
+CheckInput.prototype._isNotDecimal = function (userInput){
+  if ((userInput%1) == 0)) this.inputNrCorrect = true;
+};
+
+CheckInput.prototype._isNotLessThanOne = function (userInput){
+  if ((userInput >= 1)) this.inputNrCorrect = true;
+};
+
 CheckInput.prototype.validNumber = function (userInput){
    this._isNotString(userInput);
-   if ( (userInput >= 1) && ((userInput%1) == 0) && (isFinite(userInput) === true)) this.inputNrCorrect = true;
+   this._isNotInfinate(userInput);
+   this._isNotDecimal(userInput);
+   this._isNotLessThanOne(userInput);
    return this.inputNrCorrect
 };
 
